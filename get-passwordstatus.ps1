24c1186fd6638eb5ca183fa40be5013ff8a79c1f
Dan Lautman <34200126+dlautman@users.noreply.github.com>
2017-12-25 15:38:10 -0500

    Update get-passwordstatus.ps1

--- get-passwordstatus.ps1
+++ get-passwordstatus.ps1
@@ -1,3 +1,3 @@
 # Script pulls all enabled AD users, sorts by date password was last set, and exports to CSV.
 
-Get-ADUser -filter 'enabled -eq $true' -properties passwordlastset, passwordneverexpires, msDS-UserPasswordExpiryTimeComputed | sort-object passwordlastset | select-object Name, passwordlastset, passwordneverexpires, @{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}} | Export-csv -path c:\stage\passwordreport.csv
+Get-ADUser -filter 'enabled -eq $true' -properties passwordlastset, passwordneverexpires, msDS-UserPasswordExpiryTimeComputed | sort-object passwordlastset | select-object Name, passwordlastset, passwordneverexpires, @{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}} | Export-csv -path E:\Reports\\passwordreport.csv
